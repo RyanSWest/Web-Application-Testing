6399c451eb2bdb0b340913bd6e8aa8aa0a75277a
RyanSWest <west56309@gmail.com>
2019-11-08 00:08:57 -0600

    tests running

--- testing/src/components/helpers.js
+++ testing/src/components/helpers.js
@@ -10,9 +10,32 @@ exports.striker = function(strikes){
         strikes =0;
     }
     else{
-    return strikes ++;
+    return strikes +1;
 }
 
+} 
+
+
+exports.score = function(hits){
+
+ let points =0
+
+ if(hits>= 4){
+     points++
+ }
+ return points;
+
+
+
+}
+
+
+exports.walk = function(balls){
+   if(balls >=4){
+       balls = 0;
+   }else{
+       return balls +1;
+   }
 
 
 
--- testing/src/components/helpers.test.js
+++ testing/src/components/helpers.test.js
@@ -7,7 +7,29 @@ describe('striker', ()=> {
 
     const expected = 1;
     const actual = help.striker(0)
-    const strikeout = help.striker(3)
+     
+    expect(actual).toBe(expected)
+
   })
 
+})
+
+describe( 'score', ()=> {
+    it('scores a point on 4 hits', ()=> {
+        const expected =1;
+        const run = help.score(4)
+
+        expect(run).toBe(expected)
+
+    })
+})
+
+describe( 'walk', ()=> {
+    it('increments ball amount', ()=> {
+        const expected =0;
+        const actual = help.walk(4);
+        expect(actual).toBeExpected;
+
+        
+    })
 })
